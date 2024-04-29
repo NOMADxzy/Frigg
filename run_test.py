892adc55396ce00056fe6aa23a160a0fdc24b24b
NOMAD <3302309675@qq.com>
2024-04-29 18:05:22 +0800

    批量运行

---
+++ run_test.py
@@ -0,0 +1,29 @@
+import yaml, subprocess
+
+trace_list = ['ATT-LTE-driving', 'TMobile-LTE-driving', 'Verizon-EVDO-driving']
+step_len_ms_list = [10,20,30]
+sender_num_list = [1, 5, 20]
+
+with open('template.yaml', 'r') as file:
+    config_data = yaml.safe_load(file)
+
+command = [
+                'python',
+                './src/experiments/test.py',
+                'local',
+                '--schemes',
+                'indigo_a3c_test',
+                '--flows',
+                '1'
+            ]
+
+for _, trace in enumerate(trace_list):
+    for _, step_len_ms in enumerate(step_len_ms_list):
+        for _, sender_num in enumerate(sender_num_list):
+            config_data['trace'] = trace
+            config_data['step_len_ms'] = step_len_ms
+            config_data['flows'] = sender_num
+            with open('template.yaml', 'w') as file:
+                yaml.dump(config_data, file, default_flow_style=False)
+
+            # return_code = subprocess.call(command)
