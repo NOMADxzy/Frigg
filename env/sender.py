cccefdf2f67180ef2ba62142d26f9a73b0dbf732
NOMAD <3302309675@qq.com>
2024-04-29 15:52:47 +0800

    debug

--- env/sender.py
+++ env/sender.py
@@ -331,7 +331,7 @@ class Sender(object):
                      self.global_state.cwnd, 0, curr_ts_ms()-self.ts_first, 0, 0, 0])
 
             if len(self.metric_data) % 100 is 0:
-                if curr_ts_ms() - self.ts_first > 15000:
+                if curr_ts_ms() - self.ts_first > 12000:
                     self.output_metric()
 
             if self.debug:
